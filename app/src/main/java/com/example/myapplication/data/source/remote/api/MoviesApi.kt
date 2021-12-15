0f246b1c087b61e7f14584ffdffac7fd5fd74713
Agnaldo N Pereira <agnaldo.pereira.np.92@gmail.com>
2021-12-15 17:12:59 -0300

    Change movies url

--- app/src/main/java/com/example/myapplication/data/source/remote/api/MoviesApi.kt
+++ app/src/main/java/com/example/myapplication/data/source/remote/api/MoviesApi.kt
@@ -4,6 +4,6 @@ import com.example.myapplication.data.source.remote.entity.MovieApiResponse
 import retrofit2.http.GET
 
 interface MoviesApi {
-    @GET("/saniyusuf/406b843afdfb9c6a86e25753fe2761f4/raw/523c324c7fcc36efab8224f9ebb7556c09b69a14/Film.JSON")
+    @GET("/AgnaldoNP/UiTestExample/master/resource_json/films.json")
     suspend fun search(): List<MovieApiResponse>
 }
